4dffe23aa34da303936efeb3264daf834edf25d5
Xingu <ojyoung2490@gmail.com>
2024-05-25 03:48:53 +0900

    feat : 웹사이트 배포

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react'
+import styled, { keyframes } from 'styled-components'
+import io from 'socket.io-client'
+const socket = io(import.meta.env.VITE_API_URL) 
+
+export default function Chat() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    // 로컬 스토리지에서 초기 데이터를 비동기로 불러옵니다.
+    const classroom = localStorage.getItem('selectedClassroom')
+    socket.emit('joinRoom', classroom)
+
+    socket.on('message', (message) => {
+      const expireTime = Date.now() + 10000
+      // 서버로부터 받은 메시지가 객체 형태인 경우 message.text를 사용
+      // 문자열인 경우 바로 message를 사용
+      const text = message.text ? message.text : message
+      const newMessage = { id: Date.now(), text: text, expire: expireTime }
+      setMessages((prevMessages) => [...prevMessages, newMessage])
+    })
+
+    // 1초마다 만료된 메시지 제거
+    const interval = setInterval(() => {
+      setMessages((prevMessages) => prevMessages.filter((msg) => msg.expire > Date.now()))
+    }, 1000)
+
+    return () => {
+      clearInterval(interval) // 컴포넌트 언마운트 시 인터벌 정리
+      socket.off('message')
+    }
+  }, [])
+
+  return (
+    <Container>
+      {messages.map((msg) => (
+        <MessageBubble key={msg.id}>{msg.text}</MessageBubble>
+      ))}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column; // 메시지를 아래에서 위로 쌓도록 설정
+  align-items: flex-end;
+`
+
+const slideInAndUp = keyframes`
+  0% {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+  10% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  90% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+`
+
+const MessageBubble = styled.div`
+  max-width: 600px;
+  margin-bottom: 10px;
+  padding: 5px 10px;
+  background-color: #0164d8;
+  border: 1px solid #202c39;
+  color: #fff;
+  font-family: 'Noto Sans KR';
+  font-weight: bold;
+  border-radius: 10px;
+  font-size: 1.5rem;
+  word-wrap: break-word;
+  animation: ${slideInAndUp} 10s forwards; // 총 애니메이션 시간을 10초로 설정
+`
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Select from 'react-select'
+import styled from 'styled-components'
+
+const buildings = [
+  'AI관',
+  '비전타워',
+  '반도체대학',
+  '가천관',
+  '한의과대학',
+  '교육대학원',
+  '공과대학2',
+  '글로벌센터',
+  '예술대학2',
+  '바이오나노대학',
+  '법과대학',
+  '공과대학1'
+]
+
+function generateClassrooms(building) {
+  let classrooms = []
+  for (let floor = 1; floor <= 10; floor++) {
+    for (let room = 1; room <= 30; room++) {
+      classrooms.push(`${building}-${floor * 100 + room}`)
+    }
+  }
+  return classrooms
+}
+
+export default function Settings() {
+  const [selectedBuilding, setSelectedBuilding] = useState('')
+  const [classrooms, setClassrooms] = useState([])
+  const [selectedClassroom, setSelectedClassroom] = useState('')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (selectedBuilding) {
+      setClassrooms(generateClassrooms(selectedBuilding))
+    } else {
+      setClassrooms([])
+    }
+  }, [selectedBuilding])
+
+  const handleRegistration = () => {
+    localStorage.setItem('selectedClassroom', selectedClassroom)
+    navigate('/chat')
+  }
+
+  return (
+    <Container>
+      <Select
+        options={buildings.map((building) => ({ value: building, label: building }))}
+        onChange={({ value }) => setSelectedBuilding(value)}
+        placeholder="건물을 선택해주세요."
+      />
+      <Select
+        options={classrooms.map((classroom) => ({
+          value: classroom,
+          label: classroom.split('-')[1]
+        }))}
+        onChange={({ value }) => setSelectedClassroom(value)}
+        placeholder="호수를 선택해주세요."
+        isDisabled={!selectedBuilding}
+      />
+      <Button onClick={handleRegistration}>등록하기</Button>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  font-family: 'Noto Sans KR';
+`
+
+const Button = styled.button`
+  margin-top: 0.5rem;
+  padding: 0.5rem 1rem;
+  background-color: #0164d8;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: opacity 0.2s;
+  &:hover {
+    opacity: 0.8;
+  }
+`

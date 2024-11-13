50edeed81400dcab0eb165bf74ef7c8c9a9280e0
XinguOh <ojyoung2490@gmail.com>
2024-11-13 12:55:38 +0900

    refactor : 60초로 변경

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -108,5 +108,5 @@ const MessageBubble = styled.div`
   border-radius: 10px;
   font-size: 1.5rem;
   word-wrap: break-word;
-  animation: ${slideInAndUp} 40s forwards;
+  animation: ${slideInAndUp} 60s forwards;
 `;

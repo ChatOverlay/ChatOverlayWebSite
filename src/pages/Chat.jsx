95f1d9a2f1b39db589973417189219698602bfc5
XinguOh <ojyoung2490@gmail.com>
2024-11-14 12:33:46 +0900

    refactor: 서버 변경경

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import styled, { keyframes } from "styled-components";
 
+
 import io from "socket.io-client";
 
 const socket = io(`${import.meta.env.VITE_API_URL}`, {
